51426823115515af8d056887acb860b5ff7d89ba
NARIHY <maheninarandrianarisoa@gmail.com>
2025-12-03 10:22:58 +0100

    Modif du stock et ajustement du openapi

--- StockAPI/Program.cs
+++ StockAPI/Program.cs
@@ -4,6 +4,18 @@ using StockAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Ajouter le service CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200") // URL de ton frontend Angular
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -11,11 +23,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Activer CORS
+app.UseCors("AllowAngularApp");
 
 if (app.Environment.IsDevelopment())
 {
